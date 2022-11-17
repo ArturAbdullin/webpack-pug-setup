f67d92566faa4ca3e9f5bc4b80973583edf656a2
Artur Abdullin <artur.abdullin@gmail.com>
2022-11-17 13:57:09 +0300

    feat(webpack): setup dev server
    setup config file: devServer field
    setup config mode to development
    add 'npm run dev' script

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const PugPlugin = require("pug-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     // unlike the common webpack configs, Pug config has .pug files as the entries
     index: "./src/index.pug",
@@ -18,5 +19,22 @@ module.exports = {
         loader: PugPlugin.loader, // Pug loader
       },
     ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "build"),
+    },
+    port: 3000, // add a port to serve
+    open: true, // open browser upon serve
+    hot: true, // add HMR (hot module replacement)
+    compress: true, // enable gzip compression
+    // reload on change in (watchFiles)
+    watchFiles: {
+      paths: ["src/**/*.*"], // everything in the src folder
+      options: {
+        // It is typically necessary to set this to true to successfully watch files over a network, and it may be necessary to successfully watch files in other non-standard situations https://github.com/paulmillr/chokidar
+        usePolling: true,
+      },
+    },
   },
 };
