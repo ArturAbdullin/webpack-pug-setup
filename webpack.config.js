b711ce9841b180c86522b06a15c56ad5ab57da0d
Artur Abdullin <artur.abdullin@gmail.com>
2022-11-17 13:47:23 +0300

    feat(webpack): setup the output folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
+const path = require("path");
 const PugPlugin = require("pug-plugin");
 
 module.exports = {
   entry: {
     // unlike the common webpack configs, Pug config has .pug files as the entries
     index: "./src/index.pug",
+  },
+  output: {
+    // set the output folder to be 'build'
+    path: path.resolve(__dirname, "build"),
   },
   plugins: [new PugPlugin()],
   module: {
